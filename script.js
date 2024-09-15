22dbd8b5129dedc56e9bb7754d22352cbd853a31
Caroline Severinsen <carolineseverinsen@gmail.com>
2024-09-15 23:42:15 +0200

    detaljer om produkter

--- script.js
+++ script.js
@@ -29,56 +29,56 @@ const mainOverlayImage = document.getElementById('main-overlay-image');
           thumbnails[1].src = 'pics/skjærebrett/product1-2.jpeg';
           thumbnails[2].src = 'pics/skjærebrett/product1-3.jpeg';
           overlayTitle.textContent = 'Skjærebrett i eukalyptus og akasie';
-          overlayDescription.textContent = 'Dette er et vakkert skjærebrett laget av eukalyptus og akasie.';
-          overlayExtraDescription.textContent = 'Bestille? Send 300kr på vipps til 909 17 933. Merk med "produkt 1" og ønsket leveringsadresse.';
+          overlayDescription.textContent = 'Laget i eukalyptus og akasie tre, som er limt sammen med epoxy. Str ca l 27 x b 18 x h 1,5cm.';
+          overlayExtraDescription.textContent = 'Bestille? Send 400kr på vipps til 909 17 933. Merk med "produkt 1" og ønsket leveringsadresse.';
       } else if (productId === '2') {
         mainOverlayImage.src = 'pics/skjærebrett/product2.jpeg';
         thumbnails[0].src = 'pics/skjærebrett/product2.jpeg';
         thumbnails[1].src = 'pics/skjærebrett/product2-2.jpeg';
         thumbnails[2].src = 'pics/skjærebrett/product2-3.jpeg';
-        overlayTitle.textContent = 'Skjærebrett i eukalyptus og akasie';
-        overlayDescription.textContent = 'Et skjærebrett laget av eukalyptus og akasie.';
-        overlayExtraDescription.textContent = 'Bestille? Send 300kr på vipps til 909 17 933. Merk med "produkt 2" og ønsket leveringsadresse.';
+        overlayTitle.textContent = 'Skjærebrett i eik';
+        overlayDescription.textContent = 'str l 30 x b 29 x h 3,5cm laget i eik og oljet.';
+        overlayExtraDescription.textContent = 'Bestille? Send 600kr på vipps til 909 17 933. Merk med "produkt 2" og ønsket leveringsadresse.';
     } else if (productId === '3') {
         mainOverlayImage.src= 'pics/skjærebrett/product3.jpeg';
         thumbnails[0].src = 'pics/skjærebrett/product3.jpeg';
         thumbnails[1].src = 'pics/skjærebrett/product3-2.jpeg';
         thumbnails[2].src = 'pics/skjærebrett/product3-3.jpeg';
-        overlayTitle.textContent = 'Skjærebrett i eukalyptus og akasie';
-        overlayDescription.textContent = 'Et skjærebrett laget av eukalyptus og akasie.';
-        overlayExtraDescription.textContent = 'Bestille? Send 300kr på vipps til 909 17 933. Merk med "produkt 3" og ønsket leveringsadresse.';
+        overlayTitle.textContent = 'Skjærebrett i valnøtt og epoxy';
+        overlayDescription.textContent = 'Et skjærebrett laget av valnøtt og støpt inn med epoxy.';
+        overlayExtraDescription.textContent = 'Bestille? Send 500kr på vipps til 909 17 933. Merk med "produkt 3" og ønsket leveringsadresse.';
     } else if (productId === '4') {
       mainOverlayImage.src= 'pics/skjærebrett/product4.jpeg';
       thumbnails[0].src = 'pics/skjærebrett/product4.jpeg';
       thumbnails[1].src = 'pics/skjærebrett/product4-2.jpeg';
       thumbnails[2].src = 'pics/skjærebrett/product4-3.jpeg';
       overlayTitle.textContent = 'Skjærebrett i eukalyptus og akasie';
-      overlayDescription.textContent = 'Et skjærebrett laget av eukalyptus og akasie.';
-      overlayExtraDescription.textContent = 'Bestille? Send 300kr på vipps til 909 17 933. Merk med "produkt 4" og ønsket leveringsadresse.';
+      overlayDescription.textContent = 'L 29 x B 32x H 1,2 cm. Laget av surnet epletre, som er stabilisert og limt sammen med epoxy i fargen kaffe.';
+      overlayExtraDescription.textContent = 'Bestille? Send 1250kr på vipps til 909 17 933. Merk med "produkt 4" og ønsket leveringsadresse.';
     } else if (productId === '5') {
       mainOverlayImage.src= 'pics/skjærebrett/product5.jpeg';
       thumbnails[0].src = 'pics/skjærebrett/product5.jpeg';
       thumbnails[1].src = 'pics/skjærebrett/product5-2.jpeg';
       thumbnails[2].src = 'pics/skjærebrett/product5-3.jpeg';
       overlayTitle.textContent = 'Skjærebrett i eukalyptus og akasie';
-      overlayDescription.textContent = 'Et skjærebrett laget av eukalyptus og akasie.';
-      overlayExtraDescription.textContent = 'Bestille? Send 300kr på vipps til 909 17 933. Merk med "produkt 5" og ønsket leveringsadresse.';
+      overlayDescription.textContent = 'Str ca l 27 x b 18 x h 1,5cm. Laget i eukalyptus og akasie tre, som er limt sammen med hvit epoxy.';
+      overlayExtraDescription.textContent = 'Bestille? Send 400kr på vipps til 909 17 933. Merk med "produkt 5" og ønsket leveringsadresse.';
     } else if (productId === '6') {
       mainOverlayImage.src= 'pics/skjærebrett/product6.jpeg';
       thumbnails[0].src = 'pics/skjærebrett/product6.jpeg';
-      thumbnails[1].src = 'pics/skjærebrett/product6-2.jpeg';
+      thumbnails[1].src = 'pics/skjærebrett/product6-2.jpeg'; 
       thumbnails[2].src = 'pics/skjærebrett/product6-3.jpeg';
       overlayTitle.textContent = 'Skjærebrett i eukalyptus og akasie';
-      overlayDescription.textContent = 'Et skjærebrett laget av eukalyptus og akasie.';
-      overlayExtraDescription.textContent = 'Bestille? Send 300kr på vipps til 909 17 933. Merk med "produkt 6" og ønsket leveringsadresse.';
+      overlayDescription.textContent = 'Laget i eukalyptus og akasie tre, som er limt sammen med sort epoxy. Str ca l 27 x b 18 x h 1,5cm.';
+      overlayExtraDescription.textContent = 'Bestille? Send 400kr på vipps til 909 17 933. Merk med "produkt 6" og ønsket leveringsadresse.';
     } else if (productId === '7') {
       mainOverlayImage.src= 'pics/skjærebrett/product7.jpeg';
       thumbnails[0].src = 'pics/skjærebrett/product7.jpeg';
       thumbnails[1].src = 'pics/skjærebrett/product7-2.jpeg';
       thumbnails[2].src = 'pics/skjærebrett/product7-3.jpeg';
       overlayTitle.textContent = 'Skjærebrett i eukalyptus og akasie';
-      overlayDescription.textContent = 'Et skjærebrett laget av eukalyptus og akasie.';
-      overlayExtraDescription.textContent = 'Bestille? Send 300kr på vipps til 909 17 933. Merk med "produkt 7" og ønsket leveringsadresse.';
+      overlayDescription.textContent = 'Str l 26,5 x b 17 x h 0,7. Laget i valnøtt tre og støpt inn med epoxy.';
+      overlayExtraDescription.textContent = 'Bestille? Send 500kr på vipps til 909 17 933. Merk med "produkt 7" og ønsket leveringsadresse.';
     } else if (productId === '8') {
       mainOverlayImage.src= 'pics/skjærebrett/product8.jpeg';
       thumbnails[0].src = 'pics/skjærebrett/product8.jpeg';
@@ -86,7 +86,7 @@ const mainOverlayImage = document.getElementById('main-overlay-image');
       thumbnails[2].src = 'pics/skjærebrett/product8-3.jpeg';
       overlayTitle.textContent = 'Skjærebrett i eukalyptus og akasie';
       overlayDescription.textContent = 'Et skjærebrett laget av eukalyptus og akasie.';
-      overlayExtraDescription.textContent = 'Bestille? Send 300kr på vipps til 909 17 933. Merk med "produkt 8" og ønsket leveringsadresse.';
+      overlayExtraDescription.textContent = 'Bestille? Send 500kr på vipps til 909 17 933. Merk med "produkt 8" og ønsket leveringsadresse.';
     } else if (productId === '9') {
       mainOverlayImage.src= 'pics/skjærebrett/product9.jpeg';
       thumbnails[0].src = 'pics/skjærebrett/product9.jpeg';
@@ -94,7 +94,7 @@ const mainOverlayImage = document.getElementById('main-overlay-image');
       thumbnails[2].src = 'pics/skjærebrett/product9-3.jpeg';
       overlayTitle.textContent = 'Skjærebrett i eukalyptus og akasie';
       overlayDescription.textContent = 'Et skjærebrett laget av eukalyptus og akasie.';
-      overlayExtraDescription.textContent = 'Bestille? Send 300kr på vipps til 909 17 933. Merk med "produkt 9" og ønsket leveringsadresse.';
+      overlayExtraDescription.textContent = 'Bestille? Send 500kr på vipps til 909 17 933. Merk med "produkt 9" og ønsket leveringsadresse.';
     } else if (productId === '21') {
       mainOverlayImage.src= 'pics/kniver/kniv1.jpeg';
       thumbnails[0].src = 'pics/kniver/kniv1-2.jpeg';
@@ -102,7 +102,7 @@ const mainOverlayImage = document.getElementById('main-overlay-image');
       thumbnails[2].src = 'picser/kniver/kniv1-3.jpeg';
       overlayTitle.textContent = 'kniv i eukalyptus og akasie';
       overlayDescription.textContent = 'En kniv laget av eukalyptus og akasie.';
-      overlayExtraDescription.textContent = 'Bestille? Send 300kr på vipps til 909 17 933. Merk med "KNIV 1" og ønsket leveringsadresse.';
+      overlayExtraDescription.textContent = 'Bestille? Send 500kr på vipps til 909 17 933. Merk med "KNIV 1" og ønsket leveringsadresse.';
     } else if (productId === '22') {
       mainOverlayImage.src= 'pics/kniver/kniv2.jpeg';
       thumbnails[0].src = 'pics/kniver/knv2-2.jpeg';
